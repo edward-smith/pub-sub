975a254ddc064af9e529e296f7f19bc2d4e3faea
edward-smith <edward.smith@bydeluxe.com>
2025-09-05 15:24:06 +0200

    update

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"github.com/edward-smith/pub-sub/internal/pubsub"
+	"github.com/edward-smith/pub-sub/internal/routing"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	connstr := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connstr)
+	if err != nil {
+		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
+	}
+	fmt.Printf("connection successful...")
+	defer conn.Close()
+
+	chan, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("Failed to open a channel: %v", err)
+	}
+	defer chan.Close()
+
+	err = pubsub.PublishJSON(
+		chan,
+		routing.ExchangePerilDirect,
+		routing.PauseKey, 
+		routing.PlayingState{
+			IsPaused: true,
+		},
+	)
+	if err != nil {
+		log.Fatalf("Failed to publish message: %v", err)
+	}
+
+	fmt.Println("Pause message sent!")
 }
